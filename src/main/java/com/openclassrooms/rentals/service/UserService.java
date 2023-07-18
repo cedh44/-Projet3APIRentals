376624050c515d19deb01eb912ea49ab212a8010
cedh44 <quivole1oeuf@posteo.net>
2023-07-18 14:41:16 +0200

    Suppression de private static final DateFormat sdf car inutile

--- src/main/java/com/openclassrooms/rentals/service/UserService.java
+++ src/main/java/com/openclassrooms/rentals/service/UserService.java
@@ -5,9 +5,6 @@ import com.openclassrooms.rentals.repository.UserRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -15,7 +12,6 @@ import java.util.Date;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-    private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     //User creation
     public User createUser(User user) {
